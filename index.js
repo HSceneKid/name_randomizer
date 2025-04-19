19531f278beecaf70ad474cadea9e1cc6d377665
HSceneKid <zoemakszy@icloud.com>
2025-04-19 17:14:59 +0200

    next we can do design :D
    :)

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 console.log (":)")
-const h2 = document.querySelector("#NameH2")
-console.log (h2)
-h2.innerHTML = "blueberries"
+const button = document.querySelector("#button")
+console.log (button)
+const input = document.querySelector("#input")
+console.log (input)
+const names = ["Raven", "Crow", "Violet"]
+
+const randomName = names[Math.floor(Math.random() * names.length)];
+
+
+function randomizeName () {
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    return randomName;
+}
+
+button.addEventListener("click", function() {
+    console.log("Random Name:", randomizeName());
+    input.value=randomizeName()
+})
